e35029e883e3454229f1201aa9b853236e940b6a
Yavica <jamiemaina06@gmail.com>
2025-08-10 17:38:18 +0300

    Task 2: Reusable Card component with Tailwind CSS

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,8 +1,9 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-8 mx-auto my-20 max-w-sm sm:p-4 md:p-8">
+    <div className="bg-gray-100 p-8 mx-auto my-20 max-w-xs md:max-w-sm sm:p-4 md:p-8">
       <div className="bg-white rounded-lg shadow-lg overflow-hidden border border-gray-200 p-4">
         
+        {/* Image */}
         <div className="flex justify-center">
           <img
             src="https://randomuser.me/api/portraits/women/65.jpg"
@@ -11,10 +12,12 @@ function UserProfile() {
           />
         </div>
 
+        {/* Heading */}
         <h2 className="text-xl text-blue-800 my-4 font-bold text-center">
           Jane Doe
         </h2>
 
+        {/* Paragraph */}
         <p className="text-base text-gray-600 text-center">
           Passionate frontend developer focused on responsive UI, accessibility,
           and building delightful user experiences using React and Tailwind CSS.
