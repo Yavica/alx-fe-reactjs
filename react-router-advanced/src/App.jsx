fafec497193311b2f20f099c19ad45eed2d4e323
Yavica <jamiemaina06@gmail.com>
2025-08-24 20:58:49 +0300

    Set up React Router with Home, About, Profile

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,21 +1,27 @@
+// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
+import PostsComponent from "./components/PostsComponent";
 import Profile from "./components/Profile";
 import BlogPost from "./components/BlogPost";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-const App = () => {
+function App() {
   return (
     <Router>
       <Routes>
-        {/* Static routes */}
-        <Route path="/" element={<Home />} />
-        <Route path="/profile/*" element={<Profile />} />
-
-        {/* Dynamic route */}
+        <Route path="/" element={<PostsComponent />} />
         <Route path="/blog/:id" element={<BlogPost />} />
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;
